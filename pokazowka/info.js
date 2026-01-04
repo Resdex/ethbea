74ac62a203d039f5fae15ad1d6e4e07113530536
Resdex <118043635+Resdex@users.noreply.github.com>
2026-01-04 20:28:15 +0100

    Add files via upload

---
+++ pokazowka/info.js
@@ -0,0 +1,30 @@
+const PERSON_DATA = {
+    // Dane główne (widoczne na dowodzie)
+    name: "Zbigniew",
+    surname: "Ziobro",
+    citizenship: "POLSKIE",
+    birthDate: "16.10.1967",
+    pesel: "67294036779",
+    sex: "MĘŻCZYZNA",
+    addressDate: "2025-01-07",
+    validDate: "2035-01-07",
+    serialNumber: "EGB146052",
+    expiryDate: "2031-10-23",
+    issueDate: "2025-01-07",
+    fatherName: "ADAM",
+    motherName: "JOANNA",
+    
+    // Twoje dodatkowe dane (rozwijane)
+    familySurname: "ZIOBRO",
+    gender: "MĘŻCZYZNA",
+    fatherFamilySurname: "ZIOBRO",
+    motherFamilySurname: "KOWALSKA",
+    birthPlace: "WARSZAWA",
+    
+    // Adres i meldunek
+    address: "Wiejska 4/6/8<br>00-902, WARSZAWA",
+    registrationDate: "2025-01-07",
+    
+    // Logistyka
+    lastUpdate: "03.01.2026"
+};
